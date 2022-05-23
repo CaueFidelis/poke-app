e553521ffa89de893f9383527ab68bddb276aa14
Caue <cauefidelis@live.com>
2022-05-23 02:02:33 -0300

    Removing react query codes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { QueryClientProvider } from 'react-query';
 import Home from '../screens/Home';
 import PokemonDetails from '../screens/PokemonDetails';
-import { queryClient } from '../services/queryClients';
 import { propsNavigationStack } from './models';
 
 const Stack = createNativeStackNavigator<propsNavigationStack>();
@@ -11,12 +9,10 @@ const Stack = createNativeStackNavigator<propsNavigationStack>();
 export default function Routes() {
   return (
     <NavigationContainer>
-      <QueryClientProvider client={queryClient}>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="PokemonDetails" component={PokemonDetails} />
-        </Stack.Navigator>
-      </QueryClientProvider>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="PokemonDetails" component={PokemonDetails} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
